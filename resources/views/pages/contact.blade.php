8c7c0071adb7c2a45be84006aeb7b7dac15a85ee
Unknown <gkvaratskhelia2@gmail.com>
2017-12-06 17:23:18 +0400

    Merge branch 'check_contact'
    test_branch_double_2

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,12 +1,15 @@
-@extends('layouts.master')
-
-@section('title')
-@endsection
-<div>
-    <p>test branch</p>
-</div>
-@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
     <div>
-        <h2>test</h2>
+        new branch test
     </div>
-@endsection
+</body>
+</html>
+
