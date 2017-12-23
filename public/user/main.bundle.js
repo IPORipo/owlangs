6150205ef5c1ec8e7ee6f29268243e094619c6b7
guramsoselia <soselia.guram@gmail.com>
2017-12-23 17:41:40 +0400

    created user/wall page

--- public/user/main.bundle.js
+++ public/user/main.bundle.js
@@ -137,7 +137,14 @@ var appRouting = __WEBPACK_IMPORTED_MODULE_0__angular_router__["a" /* RouterModu
 
 /***/ }),
 
-/***/ "../../../../../src/app/home/home.component.css":
+/***/ "../../../../../src/app/home/home.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<app-header></app-header>\n<div class=\"container-fluid\" id=\"main\">\n  <div class=\"container\">\n    <div class=\"row\">\n      <div class=\"col-md-3\">\n        <div class=\"user-block\">\n          <div class=\"user-image\">\n            <a href=\"#\" class=\"user-img\"><i class=\"fa fa-camera-retro\" aria-hidden=\"true\"></i>\n            </a>\n          </div>\n          <div class=\"user-info\">\n              <h3>Guram</h3>\n              <h4>@Guram</h4>\n\n          </div>\n        </div>\n        <div class=\"navigation\">\n          <h3>Navigation</h3>\n          <ul>\n            <li><a href=\"#\"><i class=\"fa fa-book\" aria-hidden=\"true\"></i> Words</a></li>\n            <li><a href=\"#\"><i class=\"fa fa-file-excel-o\" aria-hidden=\"true\"></i>Excercices</a></li>\n            <li><a href=\"#\"><i class=\"fa fa-briefcase\" aria-hidden=\"true\"></i>Jobs</a></li>\n          </ul>\n        </div>\n      </div>\n      <div class=\"col-md-6\">\n        <div class=\"make-post\">\n          <ul class=\"nav nav-tabs\" id=\"myTab\" role=\"tablist\">\n            <li class=\"nav-item\">\n              <a class=\"nav-link active\" id=\"home-tab\" data-toggle=\"tab\" href=\"#home\" role=\"tab\" aria-controls=\"home\" aria-selected=\"true\">post a word</a>\n            </li>\n            <li class=\"nav-item\">\n              <a class=\"nav-link\" id=\"profile-tab\" data-toggle=\"tab\" href=\"#profile\" role=\"tab\" aria-controls=\"profile\" aria-selected=\"false\">post an excercise </a>\n            </li>\n            <li class=\"nav-item\">\n              <a class=\"nav-link\" id=\"contact-tab\" data-toggle=\"tab\" href=\"#contact\" role=\"tab\" aria-controls=\"contact\" aria-selected=\"false\">post a chalange</a>\n            </li>\n            <li class=\"nav-item\">\n              <a class=\"nav-link\" id=\"contact-tab\" data-toggle=\"tab\" href=\"#contact\" role=\"tab\" aria-controls=\"contact\" aria-selected=\"false\">post a job</a>\n            </li>\n          </ul>\n          <div class=\"tab-content\" id=\"myTabContent\">\n            <div class=\"tab-pane fade show active\" id=\"home\" role=\"tabpanel\" aria-labelledby=\"home-tab\">\n              <div class=\"editor d-flex\">\n                  <i class=\"fa fa-user-circle\" aria-hidden=\"true\"></i>\n                  <p>Write a word</p>\n              </div>\n            </div>\n            <div class=\"tab-pane fade\" id=\"profile\" role=\"tabpanel\" aria-labelledby=\"profile-tab\">\n                <div class=\"editor d-flex\">\n                    <i class=\"fa fa-user-circle\" aria-hidden=\"true\"></i>\n                    <p>Post an experience</p>\n                </div>\n            </div>\n            <div class=\"tab-pane fade\" id=\"contact\" role=\"tabpanel\" aria-labelledby=\"contact-tab\">\n                <div class=\"editor d-flex\">\n                    <i class=\"fa fa-user-circle\" aria-hidden=\"true\"></i>\n                    <p>Post an chalange</p>\n                </div>\n            </div>\n          </div>\n        </div>\n      </div>\n      <div class=\"col-md-3\">\n        <div class=\"friends-online\">\n          <h3>Friends online</h3>\n          <div class=\"friends-list\">\n            <div class=\"friend\">\n             <div class=\"header\">\n                <img src=\"https://pbs.twimg.com/profile_images/897379868014411777/xpJaG4a5_bigger.jpg\" alt=\"\">\n                <div class=\"details\">\n                  <h4>BBC Sport</h4>\n                  <button class=\"btn\" >Message</button>\n                </div>\n             </div>\n            </div>\n            <div class=\"friend\">\n             <div class=\"header\">\n                <img src=\"https://pbs.twimg.com/profile_images/918856427380211713/tZX3DB2h_bigger.jpg\" alt=\"\">\n                <div class=\"details\">\n                  <h4>Barnacules Nerdgasm</h4>\n                  <button class=\"btn\" >Message</button>\n                </div>\n             </div>\n            </div>\n            <div class=\"friend\">\n             <div class=\"header\">\n                <img src=\"https://pbs.twimg.com/profile_images/664516834301898752/5yEyTA0E_bigger.jpg\" alt=\"\">\n                <div class=\"details\">\n                  <h4>Josh Nerdgasm</h4>\n                  <button class=\"btn\" >Message</button>\n                </div>\n             </div>\n            </div>\n          </div>\n        </div>\n      </div>\n    </div>\n  </div>\n</div>\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/home/home.component.scss":
 /***/ (function(module, exports, __webpack_require__) {
 
 exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
@@ -145,20 +152,13 @@ exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-b
 
 
 // module
-exports.push([module.i, "", ""]);
+exports.push([module.i, "#main {\n  padding-top: 20px; }\n\n.user-block .user-image {\n  background: #9D50BB;\n  /* fallback for old browsers */\n  /* Chrome 10-25, Safari 5.1-6 */\n  background: linear-gradient(to top, #6E48AA, #9D50BB);\n  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */\n  height: 100px;\n  width: 100%;\n  display: block;\n  position: relative; }\n  .user-block .user-image .user-img {\n    width: 64px;\n    height: 64px;\n    display: block;\n    border-radius: 50%;\n    text-align: center;\n    background: #9D50BB;\n    /* fallback for old browsers */\n    /* Chrome 10-25, Safari 5.1-6 */\n    background: linear-gradient(to top, #6E48AA, #9D50BB);\n    /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */\n    position: absolute;\n    /* line-height: 63px; */\n    bottom: -34px;\n    left: 10px;\n    border: 5px solid #fff; }\n    .user-block .user-image .user-img i {\n      font-size: 31px;\n      line-height: 53px;\n      color: #fff; }\n\n.user-block .user-info {\n  background: #fff;\n  padding: 10px; }\n  .user-block .user-info h3 {\n    padding-left: 74px;\n    font-size: 21px;\n    margin-bottom: 0; }\n  .user-block .user-info h4 {\n    font-size: 15px;\n    padding-left: 71px;\n    color: #819eb5; }\n\n.make-post {\n  background: #ffff;\n  border-radius: 2px; }\n  .make-post .nav {\n    background: #f6f7f9;\n    border-top-left-radius: 2px;\n    border-top-right-radius: 2px;\n    display: -webkit-box;\n    display: -ms-flexbox;\n    display: flex;\n    -webkit-box-pack: justify;\n        -ms-flex-pack: justify;\n            justify-content: space-between;\n    padding-left: 20px;\n    padding-right: 20px; }\n    .make-post .nav li a {\n      font-size: 14px;\n      padding: 5px 10px;\n      border: none;\n      color: #6a44a2;\n      font-family: 'Lato', sans-serif;\n      font-weight: 400;\n      position: relative; }\n    .make-post .nav li a.active {\n      background: none;\n      color: #9148a7; }\n    .make-post .nav li:last-child a {\n      border-left: none !important; }\n  .make-post .tab-content {\n    height: 100px; }\n    .make-post .tab-content .editor {\n      padding-top: 15px;\n      padding-left: 30px; }\n      .make-post .tab-content .editor p {\n        margin-left: 10px;\n        color: #D4DBDD;\n        font-size: 12px;\n        padding-top: 5px; }\n      .make-post .tab-content .editor i {\n        font-size: 26px; }\n\n.navigation {\n  margin-top: 20px;\n  background: #fff;\n  border-radius: 2px; }\n  .navigation h3 {\n    font-size: 18px;\n    padding: 10px;\n    font-family: 'Lato', sans-serif;\n    font-weight: 400; }\n  .navigation ul {\n    margin-left: 0;\n    padding-left: 0; }\n    .navigation ul li {\n      list-style-type: none;\n      border-top: 1px solid #eee; }\n      .navigation ul li a {\n        padding: 20px 10px;\n        color: #6a44a2;\n        line-height: 40px;\n        font-size: 14px; }\n        .navigation ul li a i {\n          margin-right: 10px; }\n      .navigation ul li a:hover {\n        text-decoration: none; }\n\n.friends-online {\n  background: #fff;\n  border-radius: 2px; }\n  .friends-online h3 {\n    font-size: 18px;\n    font-family: 'Lato', sans-serif;\n    font-weight: 500;\n    padding: 10px;\n    padding-bottom: 0; }\n  .friends-online .friends-list .friend {\n    padding-bottom: 10px;\n    border-bottom: 1px solid #eee;\n    padding-top: 10px; }\n    .friends-online .friends-list .friend .header {\n      display: -webkit-box;\n      display: -ms-flexbox;\n      display: flex; }\n      .friends-online .friends-list .friend .header img {\n        border-radius: 50%;\n        margin: 0 10px;\n        width: 18%;\n        height: 100%; }\n      .friends-online .friends-list .friend .header h4 {\n        font-size: 14px;\n        font-family: 'Lato', sans-serif;\n        font-weight: 700; }\n      .friends-online .friends-list .friend .header button {\n        border: 1px solid #efd125;\n        border-radius: 20px;\n        margin-left: -5px;\n        color: #efd125;\n        font-size: 12px;\n        padding: 5px 20px;\n        cursor: pointer; }\n      .friends-online .friends-list .friend .header button:hover {\n        background: #f0d43473;\n        color: #fff; }\n", ""]);
 
 // exports
 
 
 /*** EXPORTS FROM exports-loader ***/
 module.exports = module.exports.toString();
-
-/***/ }),
-
-/***/ "../../../../../src/app/home/home.component.html":
-/***/ (function(module, exports) {
-
-module.exports = "<app-header></app-header>\n<p>\n  home works!\n</p>\n"
 
 /***/ }),
 
@@ -187,7 +187,7 @@ var HomeComponent = (function () {
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
             selector: 'app-home',
             template: __webpack_require__("../../../../../src/app/home/home.component.html"),
-            styles: [__webpack_require__("../../../../../src/app/home/home.component.css")]
+            styles: [__webpack_require__("../../../../../src/app/home/home.component.scss")]
         }),
         __metadata("design:paramtypes", [])
     ], HomeComponent);
@@ -236,28 +236,28 @@ var HomeModule = (function () {
 
 /***/ }),
 
-/***/ "../../../../../src/app/partials/header/header.component.css":
+/***/ "../../../../../src/app/partials/header/header.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<header>\n  <nav class=\"navbar navbar-expand-lg\">\n    <a class=\"navbar-logo\" href=\"#\"><img src=\"../images/user/logo_menu.png\" alt=\"\"></a>\n    <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n      <span class=\"navbar-toggler-icon\"></span>\n    </button>\n\n    <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">\n        <form class=\"form-inline my-2 my-lg-0\">\n            <input class=\"form-control mr-sm-2\" type=\"search\" placeholder=\"Search\" aria-label=\"Search\">\n            <button class=\"btn btn-outline-success my-2 my-sm-0\" type=\"submit\"><i class=\"fa fa-search\" aria-hidden=\"true\"></i>\n            </button>\n          </form>\n      <ul class=\"navbar-nav\">\n        <li class=\"nav-item\">\n          <a class=\"nav-link\" href=\"#\"><i class=\"fa fa-user-circle-o\" aria-hidden=\"true\"></i>\n            username\n            <i class=\"help-tip\">lore ipsum</i>\n          </a>\n        </li>\n        <li class=\"nav-item\">\n          <a class=\"nav-link\" href=\"#\"><i class=\"fa fa-signal\" aria-hidden=\"true\"></i>\n            ranking\n          </a>\n        </li>\n        <li class=\"nav-item\">\n          <a class=\"nav-link\" href=\"#\"><i class=\"fa fa-user-plus\" aria-hidden=\"true\"></i>\n          </a>\n        </li>\n        <li class=\"nav-item active\">\n          <a class=\"nav-link\" href=\"#\"><i class=\"fa fa-telegram\" aria-hidden=\"true\"></i>\n            <span class=\"sr-only\">(current)</span></a>\n        </li>\n        <li class=\"nav-item\">\n          <a class=\"nav-link\" href=\"#\"><i class=\"fa fa-globe\" aria-hidden=\"true\"></i>\n          </a>\n        </li>\n        <li class=\"nav-item dropdown\">\n          <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">\n            <i class=\"fa fa-sliders\" aria-hidden=\"true\"></i>\n          </a>\n          <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">\n            <a class=\"dropdown-item\" href=\"#\">Action</a>\n            <a class=\"dropdown-item\" href=\"#\">Another action</a>\n            <div class=\"dropdown-divider\"></div>\n            <a class=\"dropdown-item\" href=\"#\">Something else here</a>\n          </div>\n        </li>\n      </ul>\n\n    </div>\n  </nav>\n</header>\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/partials/header/header.component.scss":
 /***/ (function(module, exports, __webpack_require__) {
 
 exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
 // imports
-
+exports.push([module.i, "@import url(https://fonts.googleapis.com/css?family=Lato:300,300i,400,700|Roboto);", ""]);
 
 // module
-exports.push([module.i, "", ""]);
+exports.push([module.i, ".navbar {\n  background: #9D50BB;\n  /* fallback for old browsers */\n  /* Chrome 10-25, Safari 5.1-6 */\n  background: linear-gradient(to top, #6E48AA, #9D50BB);\n  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */ }\n  .navbar .navbar-logo {\n    text-align: center; }\n    .navbar .navbar-logo img {\n      width: 50%; }\n  .navbar form {\n    position: relative; }\n    .navbar form input {\n      height: 30px;\n      border-radius: 2px;\n      width: 450px;\n      font-size: 12px;\n      font-family: 'Lato', sans-serif;\n      font-weight: 300; }\n    .navbar form input:focus {\n      border-color: #f49500;\n      box-shadow: 0 0 0 0.2rem rgba(244, 149, 0, 0.15); }\n    .navbar form button {\n      color: #7b49b6;\n      background: #eee;\n      border: none;\n      border-radius: 2px;\n      height: 28px;\n      padding: 0 18px;\n      position: absolute;\n      right: 8px;\n      cursor: pointer; }\n    .navbar form button:hover {\n      background: #f1efef; }\n  .navbar .navbar-nav {\n    width: 100%;\n    -webkit-box-pack: end;\n        -ms-flex-pack: end;\n            justify-content: flex-end; }\n    .navbar .navbar-nav li a {\n      color: #e6ecf1;\n      position: relative; }\n      .navbar .navbar-nav li a i {\n        color: #fff; }\n      .navbar .navbar-nav li a i:hover {\n        color: #e6ecf1; }\n    .navbar .navbar-nav li a:hover {\n      color: #fff; }\n      .navbar .navbar-nav li a:hover .help-tip {\n        visibility: visible;\n        opacity: 1; }\n", ""]);
 
 // exports
 
 
 /*** EXPORTS FROM exports-loader ***/
 module.exports = module.exports.toString();
-
-/***/ }),
-
-/***/ "../../../../../src/app/partials/header/header.component.html":
-/***/ (function(module, exports) {
-
-module.exports = "<header>\n  <nav class=\"navbar navbar-expand-lg navbar-light bg-light\">\n    <a class=\"navbar-brand\" href=\"#\">Navbar</a>\n    <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n      <span class=\"navbar-toggler-icon\"></span>\n    </button>\n\n    <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">\n      <ul class=\"navbar-nav mr-auto\">\n        <li class=\"nav-item active\">\n          <a class=\"nav-link\" href=\"#\">Home <span class=\"sr-only\">(current)</span></a>\n        </li>\n        <li class=\"nav-item\">\n          <a class=\"nav-link\" href=\"#\">Link</a>\n        </li>\n        <li class=\"nav-item dropdown\">\n          <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">\n            Dropdown\n          </a>\n          <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">\n            <a class=\"dropdown-item\" href=\"#\">Action</a>\n            <a class=\"dropdown-item\" href=\"#\">Another action</a>\n            <div class=\"dropdown-divider\"></div>\n            <a class=\"dropdown-item\" href=\"#\">Something else here</a>\n          </div>\n        </li>\n        <li class=\"nav-item\">\n          <a class=\"nav-link disabled\" href=\"#\">Disabled</a>\n        </li>\n      </ul>\n      <form class=\"form-inline my-2 my-lg-0\">\n        <input class=\"form-control mr-sm-2\" type=\"search\" placeholder=\"Search\" aria-label=\"Search\">\n        <button class=\"btn btn-outline-success my-2 my-sm-0\" type=\"submit\">Search</button>\n      </form>\n    </div>\n  </nav>\n</header>\n"
 
 /***/ }),
 
@@ -286,7 +286,7 @@ var HeaderComponent = (function () {
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
             selector: 'app-header',
             template: __webpack_require__("../../../../../src/app/partials/header/header.component.html"),
-            styles: [__webpack_require__("../../../../../src/app/partials/header/header.component.css")]
+            styles: [__webpack_require__("../../../../../src/app/partials/header/header.component.scss")]
         }),
         __metadata("design:paramtypes", [])
     ], HeaderComponent);
